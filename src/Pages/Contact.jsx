35ea54d8f26df9cbc66a54bff145fc06d2b17c41
Mike Bolloskis <mpb0406@gmail.com>
2023-03-16 11:09:06 -0400

    Updated netlify form index.html

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     <div className='pt-32'>
         <h1 className='text-center text-3xl font-bold'>Contact Us</h1>
         <p className='text-center py-6'>Ask us anything.</p>
-        <form name="contact" method="POST" className="flex flex-col md:w-1/2 mx-auto">
+        <form name="contact" action="/contact" method="POST" className="flex flex-col md:w-1/2 mx-auto">
             <input type="hidden" name="form-name" value="contact" />
             <div className="flex flex-col m-2">
                 <label className="py-2 text-sm font-semibold" htmlFor="name">Name</label>
@@ -18,8 +18,7 @@ const Contact = () => {
             <div className="flex flex-col m-2">
                 <label className="py-2 text-sm font-semibold" htmlFor="message">Message</label>
                 <textarea name="message" id="message" cols="20" rows="10" required></textarea>
-            </div>
-            
+            </div>  
             <button className="primary-button w-1/3 py-2 px-4 mx-auto my-12" type="submit">Send</button>
         </form>
     </div>
